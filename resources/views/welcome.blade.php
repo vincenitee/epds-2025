e03c739fb4d266fc76150bd0c192d126db339c5c
Vincent <vincentbolinget@gmail.com>
2026-02-15 10:35:47 +0800

    feat: create auth layout component and update welcome page styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app-layout title="Welcome">
     <div class="container mx-auto px-4 py-8">
         <h1 class="text-4xl font-bold mb-4">Welcome to EPDS 2025</h1>
-        <p class="text-lg text-gray-700">This is the welcome page of the EPDS 2025 project.</p>
+        <p class="text-lg text-red-700">This is the welcome page of the EPDS 2025 project.</p>
     </div>
 </x-layouts.app-layout>
